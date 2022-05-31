696f80b27cfa07b284d53dcef47a7ade31ad248b
Boris Bukovec <104595036+BorisBukovec@users.noreply.github.com>
2022-05-31 18:10:33 +0200

    dodana tablica termin

--- dodatak_zavrsni_rad.sql
+++ dodatak_zavrsni_rad.sql
@@ -1,5 +1,5 @@
 # priprema za zavrsni_rad
-# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\botao\DZ\EdunovaPP25\SQL\dodatak_zavrsni_rad.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\botao\OneDrive\Dokumenti\zavr-ni-rad\dodatak_zavrsni_rad.sql
 drop database if exists sportski_centar;
 create database sportski_centar default charset utf8mb4;
 use sportski_centar;
@@ -10,19 +10,16 @@ create table djelatnik(
     prezime varchar (50) not null,
     kontakt varchar (20),
     email varchar (100),
-    spol varchar (20),
-    dvorana int not null
+    spol varchar (20)
 );
-create table usluga(
-    sifra int not null primary key auto_increment,
-    naziv varchar(20) not null,
-    datum_pocetka datetime,
-    vrijeme int,
-    cijena decimal (18,2),
-    djelatnik int not null,
-    korisnik int not null,
-    dvorana int not null
-);
+
+-- create table najam(
+--     sifra int not null primary key auto_increment,
+--     naziv varchar(20) not null,
+--     vrijeme int,
+--     cijena decimal (18,2) 
+-- );
+
 create table korisnik(
     sifra int not null primary key auto_increment,
     ime varchar (50) not null,
@@ -30,14 +27,25 @@ create table korisnik(
     email varchar (100) not null,
     oib char (11)
 );
+
 create table dvorana(
     sifra int not null primary key auto_increment,
     naziv varchar (50) not null,
     sport varchar (100) not null,
-    lozinka varchar (20)
+    lozinka varchar (20),
+    cijena decimal(18,2)
 );
-alter table usluga add foreign key (djelatnik) references djelatnik(sifra);
-alter table usluga add foreign key (korisnik) references korisnik(sifra);
-alter table usluga add foreign key (dvorana) references dvorana(sifra);
-alter table djelatnik add foreign key (dvorana) references dvorana(sifra);
+
+create table termin(
+    sifra int not null primary key auto_increment,
+    djelatnik int not null,
+    dvorana int not null,
+    korisnik int not null,
+    vrijeme datetime
+);
+
+
+alter table termin add foreign key (djelatnik) references djelatnik(sifra);
+alter table termin add foreign key (dvorana) references dvorana(sifra);
+alter table termin add foreign key (korisnik) references korisnik(sifra);
 
